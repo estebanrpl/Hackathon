b4691f3b6317644bfd0928f317544748f5925bcc
Bruno Janeiro <janeiro.bruno23@gmail.com>
2024-05-09 14:18:25 +0200

    feature: componennte de grafico

--- src/pages/BaseComponent.jsx
+++ src/pages/BaseComponent.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
+import { MainContent } from '../components/Main';
+import MainPage from './MainPage';
 
 const BaseComponent = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <MainPage />
     </>
   );
 };
